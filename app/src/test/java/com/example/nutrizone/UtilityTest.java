d0c5467892a39e2facbd623fd3ddbca2f6a82892
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2021-05-16 18:29:18 -0400

    Updated regex

--- app/src/test/java/com/example/nutrizone/UtilityTest.java
+++ app/src/test/java/com/example/nutrizone/UtilityTest.java
@@ -15,4 +15,16 @@ public class UtilityTest extends TestCase {
 
         assertEquals(28.0f, total_calories);
     }
+
+    public void test() {
+        String input = "Nutrition Facts 6 servings per container\n" +
+                "    Serving size 2 cakes (63g) Amount per serving Calories 280 % Daily Value* Total Fat 12g 15% Saturated Fat 7g 35% Trans Fat 0g Polyunsaturated Fat 2g Monounsaturated Fat 3g Cholesterol 10mg 3% Sodium 140mg 6% Total Carbohydrate 40g 15% Dietary Fiber Og 0% Total Sugars 28g 56% Includes 28g Added Sugars\n" +
+                "    Protein 2g Vitamin D Omcg 0% Calcium 1Omg 0% Iron 1.1mg 6% Potassium 50mg\n" +
+                "    REDMIFNOTE 5PROanurent n\n" +
+                "    MI\"DUALCAMERAL 2.000 calories a day 0% is used tor general hutrition àdvice.";
+
+        float total_calories = Utility.getQuantity(input, "Calories");
+
+        assertEquals(280.0f, total_calories);
+    }
 }
