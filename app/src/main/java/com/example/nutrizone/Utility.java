4936471c4dd97332e85ddf507123fabbd226bc9f
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2021-05-16 18:38:25 -0400

    Refined function

--- app/src/main/java/com/example/nutrizone/Utility.java
+++ app/src/main/java/com/example/nutrizone/Utility.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Utility {
     public static float getQuantity(String input, String searchString){
-        String regex = ".*"+searchString+" ([0-9]*?)(g|mg| ).*";
+        String regex = ".*"+searchString+" ([0-9]*?)[^0-9].*";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
